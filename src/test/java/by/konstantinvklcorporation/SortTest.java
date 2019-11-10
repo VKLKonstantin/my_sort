207c6761362f01736fe056bb0a7781bde7e57a67
VKLKonstantin <kos8533@yandex.ru>
2019-11-10 22:44:04 +0300

    второй тест не работает

--- src/test/java/by/konstantinvklcorporation/SortTest.java
+++ src/test/java/by/konstantinvklcorporation/SortTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static by.konstantinvklcorporation.Sort.handbook;
+import static by.konstantinvklcorporation.Sort.listK3;
 import static org.junit.Assert.*;
 
 public class SortTest {
@@ -34,4 +35,34 @@ public class SortTest {
 
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void addition(){
+        String[] k1 = {"K1","K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2"};
+        String[] k2 = {"K2","K2\\SK1", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"};
+
+        List<String> testing1 = new ArrayList();
+        List<String> testing2 = new ArrayList();
+        List<List> testing3 = new ArrayList();
+
+        for (int i = 0; i < k1.length; i++) {
+            testing1.add(k1[i]);
+        }
+        for (int i = 0; i < k2.length; i++) {
+            testing2.add(k2[i]);
+        }
+        testing3.add(testing1);
+        testing3.add(testing2);
+
+        List<List> expected = Sort.addition(listK3);
+
+        Assert.assertEquals(expected, testing3);
+
+    }
+
+    @Test
+    public void decrease(){
+
+    }
+
+
 }
