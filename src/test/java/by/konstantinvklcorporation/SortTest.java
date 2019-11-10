d5164d646337307775c321bc45de317a3b8ba536
VKLKonstantin <kos8533@yandex.ru>
2019-11-10 23:17:20 +0300

    второй и третий тест не работает наверное неправильно извлекаются списки 1 и 2 из списка 3

--- src/test/java/by/konstantinvklcorporation/SortTest.java
+++ src/test/java/by/konstantinvklcorporation/SortTest.java
@@ -61,6 +61,25 @@ public class SortTest {
 
     @Test
     public void decrease(){
+        String[] k1 = {"K1","K1\\SK2", "K1\\SK1", "K1\\SK1\\SSK2", "K1\\SK1\\SSK1"};
+        String[] k2 = {"K2","K2\\SK1", "K2\\SK1\\SSK2", "K2\\SK1\\SSK1"};
+
+        List<String> testing1 = new ArrayList();
+        List<String> testing2 = new ArrayList();
+        List<List> testing3 = new ArrayList();
+
+        for (int i = 0; i < k1.length; i++) {
+            testing1.add(k1[i]);
+        }
+        for (int i = 0; i < k2.length; i++) {
+            testing2.add(k2[i]);
+        }
+        testing3.add(testing2);
+        testing3.add(testing1);
+
+        List<List> expected = Sort.decrease(listK3);
+
+        Assert.assertEquals(expected, testing3);
 
     }
 
