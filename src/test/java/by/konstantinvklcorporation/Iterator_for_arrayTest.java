31830e39161f75fef7d0509b32c7082e800377c2
VKLKonstantin <kos8533@yandex.ru>
2019-11-12 23:12:15 +0300

    итератор и двухмерный массив

---
+++ src/test/java/by/konstantinvklcorporation/Iterator_for_arrayTest.java
@@ -0,0 +1,36 @@
+package by.konstantinvklcorporation;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class Iterator_for_arrayTest {
+
+    @Test
+    public void whenGetNext() {
+        Iterator_for_array it = new Iterator_for_array(new int[][]{{1, 2}, {3, 4}});
+
+       it.next();
+
+
+        int result = (Integer) it.next();
+
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void checkNextPosition() {
+        Iterator_for_array it = new Iterator_for_array(new int[][]{{1, 2}, {3, 4}});
+
+
+        it.next();
+        it.next();
+        it.next();
+        it.hasNext();
+
+        boolean result=it.hasNext();
+
+        assertThat(result, is(false));
+    }
+}
